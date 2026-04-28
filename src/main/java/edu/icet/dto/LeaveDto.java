ddd40ec66dab1086d27d2fadfb0e48fd29633672
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-28 22:32:50 +0530

    create LeaveDto

---
+++ src/main/java/edu/icet/dto/LeaveDto.java
@@ -0,0 +1,43 @@
+package edu.icet.dto;
+
+import edu.icet.util.LeaveStatus;
+import edu.icet.util.LeaveType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LeaveDto {
+
+    private Integer id;
+
+    @NotNull(message = "Employee ID is required")
+    private Integer employeeId;
+
+    @NotNull(message = "Leave type is required")
+    private LeaveType leaveType;
+
+    @NotNull(message = "Start date is required")
+    @FutureOrPresent(message = "Start date must be today or in the future")
+    private LocalDate startDate;
+
+    @NotNull(message = "End date is required")
+    @FutureOrPresent(message = "End date must be today or in the future")
+    private LocalDate endDate;
+
+    @Size(max = 500, message = "Reason can't exceed 500 characters")
+    private String reason;
+
+    @NotNull(message = "Leave status is required")
+    private LeaveStatus status;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+}
