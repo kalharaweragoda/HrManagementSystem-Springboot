5d63eb34b998955b8dc0ce411080ebf7aec5da40
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-29 21:34:38 +0530

    create Employee GetMapping

--- src/main/java/edu/icet/controller/EmployeeController.java
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -1,13 +1,25 @@
 package edu.icet.controller;
 
+import edu.icet.dto.EmployeeDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
 @RequestMapping("/api/employee")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
 }
