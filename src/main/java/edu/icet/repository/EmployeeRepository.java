9126fd79aa778930ec930e7059324caff2b82447
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-01 22:57:13 +0530

    create EmployeeRepository

--- src/main/java/edu/icet/repository/EmployeeRepository.java
+++ src/main/java/edu/icet/repository/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package edu.icet.repository;
 
 import edu.icet.entity.EmployeeEntity;
+import edu.icet.util.DepartmentType;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+
+    List<EmployeeEntity> findEmployeesByDepartmentType(@NotNull(message = "Department must be insert") DepartmentType departmentType);
 }
