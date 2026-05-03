cdc8f13fe427b70a06117bc8109ccb30d4aaf043
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-04 00:00:05 +0530

    implements PayrolService

--- src/main/java/edu/icet/service/impl/PayrollServiceImpl.java
+++ src/main/java/edu/icet/service/impl/PayrollServiceImpl.java
@@ -1,4 +1,12 @@
 package edu.icet.service.impl;
 
-public class PayrollServiceImpl {
+import edu.icet.service.EmployeeService;
+import edu.icet.service.PayrollService;
+import org.modelmapper.ModelMapper;
+
+public class PayrollServiceImpl implements PayrollService {
+
+    private final PayrollRepository payrollRepository;
+    private final EmployeeService employeeService;
+    private final ModelMapper modelMapper;
 }
