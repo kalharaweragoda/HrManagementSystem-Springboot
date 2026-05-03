0623644330bd11e492439c447764770dd2bb48a5
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-03 23:53:31 +0530

    add GetMapping

--- src/main/java/edu/icet/controller/PayrollController.java
+++ src/main/java/edu/icet/controller/PayrollController.java
@@ -1,4 +1,25 @@
 package edu.icet.controller;
 
+import edu.icet.dto.PayrollSendDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("api/payroll")
 public class PayrollController {
+
+    private final PayrollService payrollService;
+
+    @GetMapping
+    public ResponseEntity<List<PayrollSendDto>> getAllPayrolls() {
+        return ResponseEntity.ok(payrollService.getAllPayrolls());
+    }
 }
