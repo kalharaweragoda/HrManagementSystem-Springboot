2236c8b8f0277315c185c181a7278f030be6862a
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-08 04:47:36 +0530

    sold errors4

--- src/main/java/edu/icet/security/CustomUserDetailsService.java
+++ src/main/java/edu/icet/security/CustomUserDetailsService.java
@@ -12,6 +12,10 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if (!userRepository.existsByUsername(username)) {
