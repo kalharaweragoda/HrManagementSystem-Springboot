9776b287c2d1772613b4639a9f8f1eede02daeae
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-08 04:45:47 +0530

    sold errors3

--- src/main/java/edu/icet/config/DatabaseInitializer.java
+++ src/main/java/edu/icet/config/DatabaseInitializer.java
@@ -5,13 +5,14 @@ import edu.icet.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class DatabaseInitializer {
+public class DatabaseInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
