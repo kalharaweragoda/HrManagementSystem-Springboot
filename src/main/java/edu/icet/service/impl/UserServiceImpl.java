8716552f982ef7a47dcb6ea3b4b7bb0dd79d1540
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-08 04:23:42 +0530

    create UserServiceImpl

---
+++ src/main/java/edu/icet/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package edu.icet.service.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceImpl {
+}
